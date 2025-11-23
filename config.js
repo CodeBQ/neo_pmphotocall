1f5009dc825ec75917514b8d594352afd67a29e2
CodeBQ <135381423+CodeBQ@users.noreply.github.com>
2025-11-23 11:17:27 -0800

    Create config.js
    Add global settings

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+// Global site config
+
+const SITE_CONFIG = {
+  photographerName: "Your Name",
+  city: "Your City",
+  tagline: "Fragments of Light, Frozen in Time",
+
+  socials: {
+    instagram: "https://instagram.com/yourprofile",
+    youtube: "https://youtube.com/yourchannel",
+    twitter: "https://twitter.com/yourprofile",
+    email: "mailto:hello@example.com"
+  }
+};
